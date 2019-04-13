18b51ca985926bfb55d775e09303567d79bb071a
Joe Holt <wynatoej@gmail.com>
2019-04-13 16:33:38 -0500

    Initial Commit

---
+++ JHTools/JHTools.h
@@ -0,0 +1,19 @@
+//
+//  JHTools.h
+//  JHTools
+//
+//  Created by Joe Holt on 4/13/19.
+//  Copyright © 2019 Joe Holt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for JHTools.
+FOUNDATION_EXPORT double JHToolsVersionNumber;
+
+//! Project version string for JHTools.
+FOUNDATION_EXPORT const unsigned char JHToolsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JHTools/PublicHeader.h>
+
+
